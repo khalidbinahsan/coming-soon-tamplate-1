6fbad75b46c60f7fc3c02f5d0e1fd5068a6e6951
KHALID <khalid.freelancerbd@gmail.com>
2021-05-30 17:54:34 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,76 @@
+// countdown
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let min = document.getElementById('minutes');
+let sec = document.getElementById('seconds');
+let futureDate = new Date('June 15 2021 00:00:00');
+
+function updateCountDown() {
+    const currTime = new Date();
+    const diff = futureDate - currTime;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    min.innerHTML = m < 10 ? '0' + m : m;
+    sec.innerHTML = s < 10 ? '0' + s : s;
+}
+setInterval(updateCountDown, 1000);
+(function($) {
+    "use strict";
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit', function() {
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function() {
+        $(this).focus(function() {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        } else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+
+
+
+})(jQuery);
